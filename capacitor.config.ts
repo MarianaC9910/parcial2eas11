3fde1ffbb794085008ef5f617e52f693a317ec0d
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2025-02-20 21:41:07 -0600

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'parcial2eas11',
+  webDir: 'www'
+};
+
+export default config;
