bb8defd7a85bbf6ef8c54663e7632d18108a4ced
MarianaC9910 <158246568+MarianaC9910@users.noreply.github.com>
2025-02-20 21:44:40 -0600

    Add files via upload

---
+++ deps/ios.transition-BAEUX2X3.js
@@ -0,0 +1,15 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-YZW2PVE7.js";
+import "./chunk-6O25W2HN.js";
+import "./chunk-IDWKFNXT.js";
+import "./chunk-ACUVEYEP.js";
+import "./chunk-VEV5BJ2D.js";
+import "./chunk-BBHNRBDL.js";
+import "./chunk-OKPK4C3D.js";
+import "./chunk-ZVATTXSA.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
---
+++ deps/md.transition-KYPUB2KQ.js
@@ -0,0 +1,13 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-XO7JMSR2.js";
+import "./chunk-6O25W2HN.js";
+import "./chunk-IDWKFNXT.js";
+import "./chunk-ACUVEYEP.js";
+import "./chunk-VEV5BJ2D.js";
+import "./chunk-BBHNRBDL.js";
+import "./chunk-OKPK4C3D.js";
+import "./chunk-ZVATTXSA.js";
+export {
+  mdTransitionAnimation
+};
